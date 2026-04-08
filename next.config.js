5aa2467681a02a63f5d8d45fabdb6ec260980976
ShariqueShaikh-hyb <sharique@hybreed.co>
2026-04-08 12:14:34 +0530

    Uploading the next.config I have add the Static Build code.

--- next.config.js
+++ next.config.js
@@ -5,5 +5,15 @@ module.exports = {
   trailingSlash: true,
   distDir: 'build',
   reactStrictMode: true,
-  // output: 'export',
+
+  // Build setup for static export
+  output: 'export',
+  images: {
+    unoptimized: true,     // Static exports can't "process" images on the fly
+  },
+  // // ADD THIS LINE:
+  // basePath: '/new', 
+
+  // // AND THIS LINE (usually same as basePath):
+  // assetPrefix: '/new',
 };
